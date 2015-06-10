dbcf8e3d8715a35c7f206799c321f3d1249a3f28
erikdac <erikdac@kth.se>
2015-06-10 18:07:06 +0200

    Fixed login-bug

--- client/loginwidget.cpp
+++ client/loginwidget.cpp
@@ -4,6 +4,8 @@
 #include "json11/json11.hpp"
 
 #include <iostream>
+#include <QLineEdit>
+#include <QMessageBox>
 
 using namespace json11;
 
@@ -19,13 +21,21 @@ LoginWidget::~LoginWidget() {
 
 void LoginWidget::on_pushButton_clicked()
 {
-    login_mutex.lock();
+
+    QLineEdit * username = findChild<QLineEdit*>("username");
+    QLineEdit * password = findChild<QLineEdit*>("password");
+
     Json data = Json::object {
-        {"Username", "erik"},
-        {"Password", "no"}
+        {"Username", username->text().toStdString()},
+        {"Password", password->text().toStdString()}
     };
 
     output(data);
 
     login_mutex.lock();
+
+    QMessageBox::information(
+        this,
+        tr("Application Name"),
+        tr("An information message.") );
 }
