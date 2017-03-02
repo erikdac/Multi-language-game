4c5358018dffd7f4df223a702a449694d204c1b0
erikdac <erikdac@kth.se>
2017-03-02 17:54:07 +0100

    Renaming on server side.

--- server/gamestruct/mapstructure.go
+++ server/gamestruct/mapstructure.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"strings"
 	"strconv"
+	"math"
 	"../dbhandler"
 	"../nethandler/packets"
 	"./entity"
@@ -144,6 +145,30 @@ func RemovePlayer(player *entity.Player) (error) {
 	} else {
 		return errors.New("Player '" + player.Name + "' could not be found on the map. ")
 	}
+}
+
+func localPlayerMap(actor entity.Actor) ([]entity.Player) {
+	x, y := sliceMap(actor.X, actor.Y)
+
+	fromX := int(math.Max(float64(x-1), 0.0))
+	toX := int(math.Min(float64(x+1), float64(MAP_X-1)))
+
+	fromY := int(math.Max(float64(y-1), 0.0))
+	toY := int(math.Min(float64(y+1), float64(MAP_Y-1)))
+
+	var list []entity.Player
+
+	for i := fromX; i <= toX; i++ {
+		for j := fromY; j <= toY; j++ {
+			for _, p := range playerMap[i][j] {
+				if p.Name != actor.Name {
+					list = append(list, *p)
+				}
+			}
+		}
+	}
+
+	return list
 }
 
 func sendPlayerUpdate(player *entity.Player) {
